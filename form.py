5199e81628f6531133a670bef6920b52fa428dcb
Emogy-reunion <mv7786986@gmail.com>
2024-09-07 12:37:58 +0300

    create the login form, registration form, reverification form and password form to collect user data

---
+++ form.py
@@ -0,0 +1,61 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, InputRequired, Email, Length, Regexp, EqualTo
+
+class RegistrationForm(FlaskForm):
+    '''
+    Represents the fields of a registration form
+    '''
+    firstname = StringField('First name', validators=[DataRequired(), Length(min=2, max=30)])
+    lastname = StringField('Last name', validators=[DataRequired(), Length(min=2, max=30)])
+    email = StringField('Email', validators=[Email(), InputRequired(), Length(max=30)])
+    password = PasswordField('Password', validators=[
+        InputRequired(),
+        Length(min=8, message="Password must be at least 8 characters long."),
+        Regexp(r'(?=.*[A-Z])', message="Password must contain at least one uppercase letter."),
+        Regexp(r'(?=.*[a-z])', message="Password must contain at least one lowercase letter."),
+        Regexp(r'(?=.*\W)', message="Password must contain at least one special character.")
+        ])
+    confirmpassword = PasswordField('Confirm password', validators=[InputRequired(), EqualTo('password', message='Passwords must match!')])
+    showpassword = BooleanField('Show passwords')
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    '''
+    represents the login form fields
+    '''
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    showpassword = BooleanField('Show password')
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+class ReverificationForm(FlaskForm):
+    '''
+    represents the reverification form fields
+    '''
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    submit = SubmitField('Submit')
+
+
+class ForgotPassword(FlaskForm):
+    '''
+    form where user email is collected so as to change the password
+    '''
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+class ResetForm(FlaskForm):
+    '''
+    form to collect user's new passwords
+    '''
+
+    password = PasswordField('Password', validators=[
+        Length(min=8, message="Password must be at least 8 characters long."),
+        Regexp(r'(?=.*[A-Z])', message="Password must contain at least one uppercase letter."),
+        Regexp(r'(?=.*[a-z])', message="Password must contain at least one lowercase letter."),
+        Regexp(r'(?=.*\W)', message="Password must contain at least one special character.")
+        ])
+    confirmpassword = PasswordField('Confirm password', validators=[InputRequired(), EqualTo('password', message='Passwords must match!')])
+    showpassword = BooleanField('Show passwords')
+    submit = SubmitField('Reset password')
