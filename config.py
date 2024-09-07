45cf6b4e767fc9642853a811b44fd00a1460037a
Emogy-reunion <mv7786986@gmail.com>
2024-09-07 10:25:50 +0300

    create the application's configuration settings

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class Config:
+    """
+    Stores the application configuration settings
+    """
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
