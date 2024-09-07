d734b6f4eacaaebebe001e6a685c68bd40344379
Emogy-reunion <mv7786986@gmail.com>
2024-09-07 10:19:27 +0300

    initialize the app with configuration settings and create the sqlalchemy db instance

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ This module initializes the flask instance
 Initializes the configuration settings
 '''
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 
 # create the app instance
 app = Flask(__name__)
+
+# initialize app with configuration settings
+app.config.from_object(Config)
+
+# create the db instance
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
