440a9e0d57acccef7170ff565f1edb7db30d5ad4
Emogy-reunion <mv7786986@gmail.com>
2024-09-11 12:35:40 +0300

    fix: correct the placement of the unique constraint

--- model.py
+++ model.py
@@ -118,7 +118,7 @@ class Cart(db.Model):
     __tablename__ = 'cart'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', unique=True), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
     items = db.relationship('CartItems', lazy=True, backref='cart', cascade='all, delete-orphan')
 
