b91e215eccd7bf27dde9c309a81758cae24b9b89
Emogy-reunion <mv7786986@gmail.com>
2024-09-14 13:44:50 +0300

    fix: remove first() method since get method queries using a unique primary key

--- routes/reset.py
+++ routes/reset.py
@@ -49,7 +49,7 @@ def input_password(user_id):
         if form.validate_on_submit():
             password = form.password.data
 
-            user = db.session.get(Users, user_id).first()
+            user = db.session.get(Users, user_id)
             try:
                 user.set_password(password)
                 db.session.commit()
